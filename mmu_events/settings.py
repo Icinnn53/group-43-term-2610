d26ebb8639e948e3ed4d09a36098ef660d9ddd2c
darryl tan <dtan7728@gmail.com>
2026-07-04 17:25:41 +0800

    created database for users using the link

--- mmu_events/settings.py
+++ mmu_events/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,10 +74,10 @@ WSGI_APPLICATION = 'mmu_events.wsgi.application'
 # DATABASE
 # =========================
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
